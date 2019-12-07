ff927acf7d05760cb9a2ae5c156644e99e05a911
Jason Farrell <jfarrell@westmonroepartners.com>
2019-12-06 21:45:02 -0600

    Added task logic

--- publishHtmlTask/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const taskLib = require("azure-pipelines-task-lib");
-console.log("Hello World");
-taskLib.setResult(taskLib.TaskResult.Succeeded, "Complete");
--- publishHtmlTask/src/index.ts
+++ publishHtmlTask/src/index.ts
@@ -1,4 +1,23 @@
 import taskLib = require('azure-pipelines-task-lib');
 
-console.log("Hello World");
-taskLib.setResult(taskLib.TaskResult.Succeeded, "Complete");
+const artifactName = taskLib.getInput("artifactName", true) || "";
+const fileLocation = taskLib.getInput("htmlFilePath", true) || "";
+
+if (artifactName == "") {
+    taskLib.setResult(taskLib.TaskResult.Failed, "No Artifact Name provided");
+}
+else if (fileLocation == "") {
+    taskLib.setResult(taskLib.TaskResult.Failed, "No Artifact Path provided");
+}
+else
+{
+    try {
+        console.log('Adding attachment');
+        taskLib.addAttachment("Farrellsoft.html_artifact", "html_artifact", fileLocation);
+        taskLib.setResult(taskLib.TaskResult.Succeeded, "Complete");
+    }
+    catch (error) {
+        console.log(error);
+        taskLib.setResult(taskLib.TaskResult.Failed, error);
+    }
+}
